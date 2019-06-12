005d886f0618830dafff276a5b7156a38dcbcbbb
liushixunryan <996822526@qq.com>
2019-06-12 11:21:20 +0800

    commitindex

---
+++ js/JS.js
@@ -0,0 +1,14 @@
+new Vue({
+    el:".bigbox",
+    data() {
+        return {
+            activeIndex: '1',
+            activeIndex2: '1'
+        };
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        }
+    }
+});
